141ca7c06dd1211178f4a951bee46401831371b4
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-05-02 08:47:28 -0500

    starting the basics

---
+++ basics.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+/*
+ * Author: Quinn McClure
+ * Purpose: Getting familiar with the bare bones basics of Swift
+ */
+
+
+//Naming conventions: camelCase
+//ex: var helloWorld
+
+//print ex
+print("Hello World")
+
+
+
